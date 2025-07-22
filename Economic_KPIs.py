b0734b663d2270f4db5dca87796631133e3e2ed7
dgiannakakos <dgiannakakos@epu.ntua.gr>
2025-07-22 16:20:40 +0300

    Initial commit for FastAPI project

---
+++ Economic_KPIs.py
@@ -0,0 +1,151 @@
+Economic_KPIs = {
+
+"Budget Management & Optimization (per project/technology)": {
+"EBO1": {
+    "Name": "Operational Costs Avoidance",
+    "Primary use": "Performance",
+    "Units of measurement": "EUR/kWh",
+    "Description": "Avoided operating costs by setting the CEL/REV-Lab."
+        },
+"EBO2": {
+    "Name": "Levelised Cost of Electricity",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/kWh",
+    "Description": "Average total cost to build and operate a power-generating asset over its lifetime divided by the total energy output of the asset over that lifetime."
+        },
+"EBO3": {
+    "Name": "Levelised Cost of Heat",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/kWh",
+    "Description": "Average total cost to build and operate a heat-generating asset over its lifetime divided by the total energy output of the asset over that lifetime."
+        },
+"EBO4": {
+    "Name": "Levelised Cost of Hydrogen",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/kg",
+    "Description": "A variable that indicates how much it costs to produce 1 kg of Hydrogen."
+        },
+"EBO5": {
+    "Name": "Levelised Cost of Charging",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/kWh",
+    "Description": "Average total cost charging an EV divided by the total energy charged."
+        },
+"EBO6": {
+    "Name": "Capital expenditure",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR",
+    "Description": "Total value of financial investments or capital expenditures."
+        },
+"EBO7": {
+    "Name": "Operational expenditure",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/kWh , EUR",
+    "Description": "Sum of total operational costs expenditures on site, planned O&M and unscheduled maintenance."
+        },
+"EBO8": {
+    "Name": "Maintenance costs",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/kWh , EUR",
+    "Description": "Total maintenance expenditures to each technology provider (or demo partners)."
+        },
+"EBO9": {
+    "Name": "Financing costs",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR",
+    "Description": "Total financing expenditures."
+        },
+"EBO10": {
+    "Name": "Resource cost per ton",
+    "Primary use": "Performance",
+    "Units of measurement": "EUR",
+    "Description": "The average cost incurred for handling or processing each metric ton of organic materials."
+        },
+"EBO11": {
+    "Name": "Land cost",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/m²",
+    "Description": "The cost of the land required for the installation of the RES technologies."
+        },
+"EBO12": {
+    "Name": "Grid cost (MV, HV)",
+    "Primary use": "Planning",
+    "Units of measurement": "EUR/MW",
+    "Description": "Cost associated with MV upgrades for RES connection to the distribution network."
+        },
+"EBO13": {
+    "Name": "Monetisation potential of EV flexibility",
+    "Primary use": "Performance",
+    "Units of measurement": "%",
+    "Description": "Increase in the monetisation potential of EV flexibility and V2x compared to current levels."
+        },
+"EBO14": {
+    "Name": "Implementation cost share",
+    "Primary use": "Performance",
+    "Units of measurement": "EUR",
+    "Description": "Represents the cost of a project per community when compared to the total cost for all the communities (can be adapted to other geographical scopes, for instance, neighbourhood)."
+        },
+},
+
+"Financial viability & Capital Efficiency":{
+"EFV1": {
+    "Name": "Net Present Value",
+    "Primary use": "Performance",
+    "Units of measurement": "EUR",
+    "Description": "Present value of the entire project benefits including the design, installation, and execution phases. It represents the total money profitability of the project."
+        },
+"EFV2": {
+    "Name": "Life-cycle cost of energy generation",
+    "Primary use": "Performance",
+    "Units of measurement": "Years",
+    "Description": "The sum of all the costs throughout the lifetime of the energy investment, normalised to the energy generated. Total CAPEX & OPEX per electric and thermal generation, divided per the overall electric production and thermal production, in the technical/useful lifetime of the overall project (approx. 20/25 years)."
+        },
+"EFV3": {
+    "Name": "Internal Rate of Return (IRR)",
+    "Primary use": "Performance",
+    "Units of measurement": "%",
+    "Description": "The interest rate at which the NPV of the investment is zero."
+        },
+"EFV4": {
+    "Name": "Payback Period",
+    "Primary use": "Performance",
+    "Units of measurement": "years",
+    "Description": "The period of time needed for the cumulative gains from an investment to equal the cumulative cost."
+        },
+"EFV5": {
+    "Name": "Social Economic/ prosperity indicator",
+    "Primary use": "Performance",
+    "Units of measurement": " ",
+    "Description": "The amount of value globally produced by the project: quantity of euros produced by each euro invested to build the project."
+        },
+"EFV6": {
+    "Name": "Amount of financing offered to support participants in the community engagement process",
+    "Primary use": "Performance",
+    "Units of measurement": "EUR",
+    "Description": "This KPI measures the total monetary amount of financing provided to support individuals or groups involved in the community engagement process."
+        },
+"EFV7": {
+    "Name": "Emission Return on Invested Capital",
+    "Primary use": "Performance",
+    "Units of measurement": "EUR",
+    "Description": "Capital Invested per Kt CO2 reduced."
+        },
+},
+
+"Direct cost savings for CEL members":{
+"ECS1": {
+    "Name": "Energy bills' reduction",
+    "Primary use": "Tracking or Planning",
+    "Units of measurement": "kEUR/consumer",
+    "Description": "Economic saving in the energy bills for the citizens after setting the CEL/REV-Lab."
+        },
+"ECS2": {
+    "Name": "Savings in fuel costs when using EVs instead of conventional",
+    "Primary use": "Tracking",
+    "Units of measurement": "kEUR/vehicle",
+    "Description": "The savings in fuel costs associated with the usage of electricity, particularly for EVs, compared to conventional vehicles (e.g., gasoline or diesel vehicles)."
+        },
+},
+
+}
+
---
+++ README.md
@@ -0,0 +1,66 @@
+# REV Readiness Assessment fastAPI
+
+This project provides the endpoints and functionality of REV readiness assessment tool in a FastAPI-based backend 
+
+- KPIs: User chooses any of the available KPIs and can add some of his own (must belong in existing categories e.g. social etc.)
+- A score is calculated for each KPI category
+- User can edit or delete any of the added KPIs
+
+- Barriers and disadvantages: User chooses any of the available barriers 
+- A score is calculated for each barrier persona (category)
+
+- Climate vulnerability: User chooses any of the available assets and weather variables and gets climate_vulnerability insights    
+
+- Weather variables: User gets a description of all available weather variables
+##  Features
+
+- @app.get("/kpis")
+  Gets all KPI items  
+
+- @app.get("/barriers_disadvantages")
+  Gets all barriers and disadvantages items 
+
+- @app.get("/weather_variables")
+  Gest all weather variables items
+
+- @app.get("/climate_vulnerability")
+  Gets all climate vulnerability items
+
+- @app.get("/custom_kpis")
+  Gets all KPIs added by the user
+
+- @app.get("/kpi/{category}/{kpi_id}")
+  Gets one specific KPI from existing/added
+
+- @app.get("/barrier/{persona}/{barrier_id}")
+  Gets one specific barrier
+
+- @app.get("/weather_variable/{variable_name}")
+  Gets one specific weather variable
+
+- @app.post("/add_custom_kpi")
+  Adds a custom (user added) KPI to one of the existing categories (social, economic etc.)
+
+- @app.post("/kpis_score")
+  Calculates the score of the KPI categories
+
+- @app.post("/barriers_score")
+  Calculates the score of the barriers personas
+
+- @app.post("/climate_vulnerability_text")
+  Posts the climate vulnerability insights of the chosen assets
+
+- @app.put("/edit_custom_kpi/{category}/{subcategory}/{kpi_id}")
+  Edits one of the custom (user added) KPIs
+
+- @app.delete("/delete_custom_kpi/{category}/{subcategory}/{kpi_id}")
+  Deletes one of the added (user added) KPIs 
+
+##  Requirements
+
+See requirements.txt file
+
+## Test the endpoints through FastAPI - Swagger UI
+- Run in terminal:  "python -m uvicorn main:app --reload" OR "py -m uvicorn main:app"
+- Visit URL in which uvicorn runs
+- add "/docs" at the end of the URL
